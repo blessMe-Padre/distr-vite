7b66821d3be7e6ef2838a84e61011d4cca2c9e71
Сергей Субботин <zarodiny@yandex.ru>
2024-08-27 21:28:15 +1000

    init

---
+++ vite-project/main.js
@@ -0,0 +1,9 @@
+import './style.scss';
+import { init } from '../vite-project/js-module/init';
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('подключен скрипт main.js');
+
+  init();
+  // DOMContentLoaded
+});
