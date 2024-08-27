e2a4132fc7ea88f281b6495862d0265228488e79
Сергей Субботин <zarodiny@yandex.ru>
2024-08-28 08:13:18 +1000

    feat: добавляет стили

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -1,8 +1,37 @@
-export default {
-  content: ["./*.{html,js}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+  ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '768px',
+      'md': '1024px',
+      'lg': '1280px',
+      'xl': '1440px',
+    },
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+
+      // ---------------------------------
+      'default-black': '#000000',
+      'dark-blue': '#152033',
+      'blue': '#192436',
+      'orange': 'orange',
+    },
+
+    container: {
+      padding: '20px',
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        'bodyFont': ['archangelsk', 'regular'],
+      }
+    }
   },
   plugins: [],
 }
-
